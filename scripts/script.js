39b5ea73b51de3c4d9a2980de91fd21d5da8f07d
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2024-01-12 14:39:36 +0200

    Added Necessary files

---
+++ scripts/script.js
@@ -0,0 +1,63 @@
+//part one
+class Shape {
+  constructor(name, sides, sideLength) {
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+  }
+
+  calcPerimeter() {
+    const perimeter = this.sides * this.sideLength;
+    console.log(`Perimeter of ${this.name}: ${perimeter}`);
+  }
+}
+
+const square = new Shape("square", 4, 5);
+square.calcPerimeter();
+
+const triangle = new Shape("triangle", 3, 3);
+triangle.calcPerimeter();
+
+//part two
+class Square extends Shape {
+  constructor(sideLength) {
+    super("square", 4, sideLength);
+  }
+
+  calcArea() {
+    const area = this.sideLength * this.sideLength;
+    console.log(`Area of square: ${area}`);
+  }
+}
+
+const square2 = new Square(5);
+square2.calcPerimeter();
+square2.calcArea();
+
+//part three
+class Triple {
+  static customName = "Tripler";
+  static description = "I triple any number you provide";
+
+  static calculate(n = 1) {
+    return n * 3;
+  }
+}
+
+class SquaredTriple extends Triple {
+  static description = "I square the triple of any number you provide";
+  static longDescription;
+
+  static calculate(n) {
+    return super.calculate(n) ** 2;
+  }
+}
+
+console.log(Triple.description);
+console.log(Triple.calculate());
+console.log(Triple.calculate(6));
+
+console.log(SquaredTriple.calculate(3));
+console.log(SquaredTriple.description); 
+console.log(SquaredTriple.longDescription); 
+console.log(SquaredTriple.customName); 
